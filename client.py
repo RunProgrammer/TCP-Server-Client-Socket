9de289dd04feade74b6b4cf01e405e144f7730cd
Run_1404 <68385606+RunProgrammer@users.noreply.github.com>
2025-05-05 22:20:26 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,35 +1,71 @@
 import socket
-import time
+from colorama import init
+from termcolor import colored
+init(autoreset=True) #Initialize colorama to reset colors after each print
 
 
 HEADER = 128 #Header size for the message
 FORMAT = 'utf-8' #Format of the message
-#TODO:DEFINING PROTS AND SERVER ADDRESS
+
 PORT = 5000
 DISCONNECT = "!DISCONNECT!"
-SERVER = "###.###.###.###"
+SERVER = "169.254.74.144"
 ADDR = (SERVER, PORT)
 
 CLIENT_SOC = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a TCP socket
 CLIENT_SOC.connect(ADDR) #Connects to the server with the defined address and port
 
 
+def msgFormatter(msg):
+    message = msg.encode(FORMAT) 
+    msgLen = len(message)
+    print(f"Length of the message is : {msgLen}")
+    sendLen = str(msgLen).encode(FORMAT)
+    sendLen += b' ' * (HEADER - len(sendLen)) 
+    
+    return sendLen, message
+
+
 def send(msg):
 
-    message = msg.encode(FORMAT)
-    msgLen = len(message) #Calculates the length of the message
-    print(msgLen)
-    sendLen = str(msgLen).encode(FORMAT)
-    sendLen += b' ' * (HEADER - len(sendLen))
+    sendLen, message = msgFormatter(msg) #Format the message to be sent
     CLIENT_SOC.send(sendLen)
     CLIENT_SOC.send(message)
-
     print("Message Sent !")
 
     MsgRecv = CLIENT_SOC.recv(2048).decode(FORMAT)
     print(MsgRecv) 
 
-send("First Message :)")
+
+def disconnect(msg):
+
+    sendLen, message = msgFormatter(msg) #Format the message to be sent
+    CLIENT_SOC.send(sendLen)
+    CLIENT_SOC.send(message)
+    CLIENT_SOC.close()
+    print(colored("Disconnected from the server ...", 'red'))
+
+        
+
+
+if __name__ == "__main__":
+    while True:
+        print(colored("WELCOME TO THE CLIENT SIDE", 'green'))
+        print("")
+        print("1. Send a message")
+        print("2. Disconnect from the server")
+        print("")
+        askInput = input(colored("Select an option (1-3): ", 'yellow'))
+        if askInput == "1":
+            msg = input("Enter your message: ")
+            send(msg)
+        elif askInput == "2":
+            disconnect(DISCONNECT)
+            break
+        else:
+            print(colored("Invalid option. Please try again.", 'red'))
+    
+        
 
 
 
--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 import socket
 import threading
-import os
-import time
-
-
+from datetime import datetime
+from colorama import init
+from termcolor import colored
+init(autoreset=True) 
 
 HEADER = 128 #Header size for the message
 FORMAT = 'utf-8' #Format of the message
@@ -17,33 +17,45 @@ DISCONNECT = "!DISCONNECT!"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a TCP socket
 server.bind(Addr)
 
+def loggHandler(logInfo):
+    with open("serverLog.log", "a+") as logFile:
+        logFile.write(f"{datetime.now()} :- {logInfo} \n")
+        print("Logged")
+        
 
 
 def handleConnection(conn , addr):
     
-    print(f"[NEW CONNECTION] {addr} connected. \n")
+    print(colored(f"[NEW CONNECTION] {addr} connected. \n", 'green'))
     # print("Hi there! : ) \n")
     
-    connectedCheck = True
-    while connectedCheck:
-        #Read the header(length of the msg)
-        msgLength = conn.recv(HEADER).decode(FORMAT) #recives lenth in bytes of padding
-        
-        if msgLength:
-            msgLength = int(msgLength.strip())
-            #Reads the main body of the msg
-            msg = conn.recv(msgLength).decode(FORMAT)
-        
+    loggHandler(f"{addr} connected succesfully ..") #Logs the connection 
+    
+    try:
+        connectedCheck = True
+        while connectedCheck:
+            #Read the header(length of the msg)
+            msgLength = conn.recv(HEADER).decode(FORMAT) #recives lenth in bytes of padding
 
-        print(f"{addr} says : {msg} \n")
+            if msgLength:
+                msgLength = int(msgLength.strip())
+                #Reads the main body of the msg
+                msg = conn.recv(msgLength).decode(FORMAT)
 
-        conn.send("Message Recived".encode(FORMAT)) 
 
-        connectedCheck = False
-        if msg == DISCONNECT:
-            connectedCheck = False
-        
-    conn.close() #Closes the connection with the client
+            print(f"{addr} says : {msg} \n")
+
+            if msg == DISCONNECT:
+                connectedCheck = False
+                loggHandler(f"{addr} disconnected succesfully !!")
+                conn.close() #Closes the connection with the client
+            else:
+                conn.send("Message Recived".encode(FORMAT)) 
+    
+    except Exception as e:
+        print(colored(f"[ERROR] {addr} disconnected with error : {e}", 'red'))
+        loggHandler(f"{addr} disconnected with error : {e}")
+        conn.close()
             
 
 
@@ -54,22 +66,19 @@ def startServer():
     while True:
         #It returns the connection (port and address) of the client
         connection , addr = server.accept() #Accepts the connection from the clients
-        
-        # print(addr)
-        
-
         #creates a thread for each connection
         threadIt = threading.Thread(target=handleConnection, args=(connection , addr)) 
 
-        #Starting the thread
         threadIt.start()
 
-        #Prints the number of active connections
-        # print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1} \n") 
-        #As the thread already starting one connection , we need to subtarct it by 1
+        '''
+        if (threading.active_count() - 1) == 1:
+            break
+            exit(0)
+        else:
+            continue
+        '''
 
-
-
-print(f"<STARTING SERVER> .... :) {Addr} \n")
+print(colored(f"<STARTING SERVER> .... :) {Addr} \n", 'green'))
 print("")
 startServer()
